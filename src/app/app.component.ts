b836fb96168cddb9af033144f3bddf4f8c56a9ec
Ashish <dev.ashishkumar0512@gmail.com>
2024-02-25 11:34:26 +0530

    angular scroll single page app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { DOCUMENT } from '@angular/common';
 import { Component } from '@angular/core';
+import { Inject } from '@angular/core';
+import { PageScrollService } from 'ngx-page-scroll-core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'single-page-scroll-app';
+  activeSection = 1;
+  constructor(
+    private pageScrollService: PageScrollService,
+    @Inject(DOCUMENT) private document: any
+  ) {}
+
+  fullPageScroll(e: HTMLElement, i:any) {
+    this.pageScrollService.scroll({
+      scrollTarget: e,
+      document: this.document,
+    });
+    this.activeSection = i;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPageScrollCoreModule.forRoot({ duration: 500 })
   ],
   providers: [],
   bootstrap: [AppComponent]
